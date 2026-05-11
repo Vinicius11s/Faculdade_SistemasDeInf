650d59ace551704465540969cf315b4428cc4ce6
Vinicius11s <vinihortasantana@gmail.com>
2026-05-11 13:37:33 -0300

    Aula 09 - DTOs e Refatoração de Endpoints
    Criação das classes CursoDTO e ProfessorDTO para separar a camada de apresentação do   modelo de domínio.
    Adicionados endpoints GET /curso/{id} e GET /professor/{id} retornando os
    respectivos DTOs.
    ProfessorDTO inclui a lista de cursos vinculados ao professor.
    Corrigidas dependências inválidas no pom.xml e versão do Spring Boot ajustada para
      3.4.5

---
+++ 7-Termo-SIS/ProgramaçãoWebJava/ensino/src/main/java/com/fachiano/ensino/model/Curso.java
@@ -0,0 +1,82 @@
+package com.fachiano.ensino.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Curso {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private int cargaHoraria;
+    
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private Professor professor;
+    
+    @ManyToMany(mappedBy = "cursos")
+    private List<Aluno> alunos = new ArrayList<>();
+
+    public Curso() {}
+    
+	public Curso(String nome, int cargaHoraria) {
+		super();
+		this.nome = nome;
+		this.cargaHoraria = cargaHoraria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+    
+	
+    
+
+}
